ba233f9e4db8b0bb25e1508e575c6e6df8c0d03c
Gaëlle <gaelleboucher.dev@gmail.com>
2025-04-09 20:05:17 +0200

    taille logo, suppression p

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,10 @@ const Header: React.FC = () => {
         <header className="w-full bg-white shadow-sm py-4 px-6 flex items-center justify-between">
             <div className="flex items-center gap-3">
                 <img
-                src="/src/assets/logo-gbwebassist.png"
+                src="/assets/logo-gbwebassist.png"
                 alt="Logo GB WebAssist"
-                className="w-10 h-10 object-contain"
+                className="w-40 h-40 object-contain"
                 />
-                <div>
-                <h1 className="text-xl font-bold">Gaëlle</h1>
-                <p className="text-sm text-gray-500">Je crée des sites qui racontent votre histoire</p>
-                </div>
             </div>
         </header>
     )
