d139460e04c275bd0aab11edc62201b5f728ae0f
Gaëlle <gaelleboucher.dev@gmail.com>
2025-04-12 19:59:30 +0200

    config tailwind v4.1 + css du compo LinksPagegit add .git add . (ENFIN)

--- src/components/LinksPage.tsx
+++ src/components/LinksPage.tsx
@@ -22,20 +22,19 @@ const links = [
 
 const LinksPage: React.FC = () => {
   return (
-    <div className="min-h-screen w-full text-white flex flex-col items-center justify-between"
-        style={{ background: 'linear-gradient(to bottom right, #2f3239, #1ab1dd, #1fcfb7)' }}>
+    <div className="min-h-screen w-full bg-background-body text-textlight flex flex-col items-center justify-between">
       <Header />
 
       <main className="flex flex-col items-center justify-center flex-1 px-4 w-full">
         <section className="flex flex-col items-center justify-center w-full max-w-md px-4">
-        <img
-          src="/assets/photo-gaelle.jpg"
-          alt="Photo de Gaëlle"
-          className="w-28 h-28 rounded-full object-cover shadow-xl mb-4 border-4 border-[primary]"
-        />
+          <img
+            src="/assets/photo-gaelle.jpg"
+            alt="Photo de Gaëlle"
+            className="w-28 h-28 rounded-full object-cover shadow-lg mb-4 border-4 border-secondary"
+          />
 
-          <h1 className="text-3xl font-bold mb-1 text-text-light">Gaëlle</h1>
-          <p className="text-text-light/80 text-sm mb-6 flex items-center gap-2 text-center">
+          <h1 className="text-3xl font-bold mb-1">Gaëlle</h1>
+          <p className="text-sm mb-6 flex items-center gap-2 text-center">
             Je crée des sites qui racontent votre histoire
             <span>📖</span>
           </p>
