644932f0a26c4eefd99e839132b4cfbaf7a7be5d
Gaëlle <gaelleboucher.dev@gmail.com>
2025-04-13 10:11:04 +0200

    logo + gros, ajout scale + slogan

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className="w-full bg-white shadow-sm py-4 px-6 flex items-center justify-between">
-            <div className="flex items-center gap-3">
-                <img
+        <header className="w-full bg-background-dark py-6 px-6 flex flex-col sm:flex-row justify-center items-center gap-4">
+            <img
                 src="/assets/logo-gbwebassist.png"
                 alt="Logo GB WebAssist"
-                className="w-40 h-40 object-contain"
-                />
+                className="h-25 w-auto transition-transform duration-300 ease-in-out hover:scale-110"
+            />
+            <div className="text-center sm:text-left">
+                <h1 className="text-2xl font-semibold text-accent1 transition-transform duration-300 ease-in-out hover:scale-110">
+                GB WebAssist
+                </h1>
+                <p className="text-sm text-textlight transition-transform duration-300 ease-in-out hover:scale-110">Des sites qui racontent votre histoire</p>
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
