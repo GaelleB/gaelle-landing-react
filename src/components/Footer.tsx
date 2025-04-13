9e1d4727e85acc45e8eb860c41c4c67abdb6c041
Gaëlle <gaelleboucher.dev@gmail.com>
2025-04-13 10:34:20 +0200

    text xl sur le footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="text-center py-6 text-sm text-gray-400">
-            <p>Une page signée avec amour et un peu de code par GB WebAssist - 2025 ✨</p>
+        <footer className="w-full bg-background-dark text-textlight py-10 text-center text-sm">
+        <p className="text-xl opacity-80">
+            Une page signée avec amour et un peu de code par <span className="text-accent1 font-semibold">GB WebAssist</span> – 2025 ✨
+        </p>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
